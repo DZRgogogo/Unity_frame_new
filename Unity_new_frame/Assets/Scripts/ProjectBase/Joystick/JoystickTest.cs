5b6df70649b5d2575094cba075e06adde8b06a0d
dzrcode <541456313@qq.com>
2020-11-23 00:12:05 +0800

    更新了摇杆控制模块

---
+++ Unity_new_frame/Assets/Scripts/ProjectBase/Joystick/JoystickTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///挂在被移动的物体上
+///</summary>
+public class JoystickTest : MonoBehaviour 
+{
+    private Vector3 dir;
+    private void Start()
+    {
+        EventCenter.GetInstance().AddEventListener<Vector2>("Joystick", CheckDirChange);
+    }
+    private void Update()
+    {
+        this.transform.Translate(dir, Space.World);
+    }
+    private void CheckDirChange(Vector2 dir)
+    {
+        this.dir.x = dir.x;
+        this.dir.z = dir.y;
+    }
+}
+
